b2ea550a608e6ceba511c2d4c009ac37ca091538
syfless <syfless1999@gmail.com>
2021-03-02 21:10:22 +0800

    Feat: http network layer 20%

--- src/controller/network.ts
+++ src/controller/network.ts
@@ -1,13 +1,38 @@
 import { Request, Response } from 'express';
 import * as network from 'src/type/network';
 import networkData from 'src/mock/networkData.json';
+import { retrieveNetworkAndEdgeByLevelAndLabel } from 'src/service/network';
 
-
+/**
+ * http [ temporary ]
+ * @param req 
+ * @param res 
+ * @param next 
+ */
 export const retrieve = async (req: Request, res: Response, next: (error: Error) => any) => {
   try {
     res.json({
       message: 'success',
       data: networkData as network.LayerNetwork,
+    })
+  } catch (error) {
+    next(error);
+  }
+}
+
+/**
+ * http
+ * @param req 
+ * @param res 
+ * @param next 
+ */
+export const retrieveLayer = async (req: Request, res: Response, next: (error: Error) => any) => {
+  try {
+    const { params: { datasourceName }, query: { level } } = req;
+    const layer = retrieveNetworkAndEdgeByLevelAndLabel(datasourceName, Number(level));
+    res.json({
+      message: 'success',
+      data: [layer],
     })
   } catch (error) {
     next(error);
--- src/route/network.ts
+++ src/route/network.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import * as  networkController from 'src/controller/network';
+import * as networkController from 'src/controller/network';
 
 const router = Router();
 
 router.get('/', networkController.retrieve);
+router.get('/:datasourceName', networkController.retrieve);
 
 export default router;
